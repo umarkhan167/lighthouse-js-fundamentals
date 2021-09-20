82cc128f41b1f39c942cb67cf863632bbaa79d9e
Umar Khan <umarkhan167@outlook.com>
2021-09-20 08:48:42 -0400

    git push

--- conditionals.js
+++ conditionals.js
@@ -7,9 +7,11 @@ if (isCitizen && age > 18) {
 
 
 if (temperature < -40 || temperature > 40) {
-  console.log("Maybe going outside isn't such a great idea…");
+  console.log("Maybe going outside isn't such a great idea…")              ;
 }
 
+console.log("hello")
+                                                                              
 
 if (!raining) {
   console.log("Leave your umbrella at home!");
