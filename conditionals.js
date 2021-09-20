29bf89c8a1b8850882ad3bc9675c21e6a1aad3e8
Umar Khan <umarkhan167@outlook.com>
2021-09-20 09:04:46 -0400

    Initial commit

--- conditionals.js
+++ conditionals.js
@@ -8,13 +8,35 @@ if (isCitizen && age > 18) {
 
 if (temperature < -40 || temperature > 40) {
   console.log("Maybe going outside isn't such a great idea…")              ;
-}
-
-console.log("hello")
-                                                                              
+}                                                                
 
 if (!raining) {
   console.log("Leave your umbrella at home!");
 }
+
+// Logical Operators
+
+//example 1
+const isCitizen = true;
+const age = 26;
+
+if (isCitizen && age > 18) {
+  console.log("You are eligible to vote.");
+}
+
+//example 2
+const temperature = -45;
+
+if (temperature < -40 || temperature > 40) {
+  console.log("Maybe going outside isn't such a great idea…");
+}
+
+//example 3
+const raining = false;
+
+if (!raining) {
+  console.log("Leave your umbrella at home!");
+}
+
 
 
